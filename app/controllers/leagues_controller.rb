33a39f52814893ea570367de2a95a1f127aacc7d
Kaja Witek <kaja.wasiolek@gmail.com>
2023-01-21 19:20:38 +0100

    feat: add updating league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -9,16 +9,29 @@ class LeaguesController < ApplicationController
   end
 
   def create
-    @league = League.new(league_params)
-    if @league.save
+    league = League.new(league_params)
+    if league.save
       redirect_to leagues_path, notice: "League was successfully created."
     else
-      redirect_to leagues_path, notice: "League wasn't created."
+      render :new, locals: { league: league }, status: :unprocessable_entity
     end
   end
 
   def show
     render :show, locals: { league: League.find(params[:id]) }
+  end
+
+  def edit
+    render :edit, locals: { league: League.find(params[:id]) }
+  end
+
+  def update
+    league = League.find(params[:id])
+    if league.update(league_params)
+      redirect_to leagues_path, notice: "League was successfully updated."
+    else
+      render :edit, locals: { league: league }, status: :unprocessable_entity
+    end
   end
 
   private
